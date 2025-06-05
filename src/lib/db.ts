ed2c10d23b3d3f42853c262cb845e53a93361a64
Ilia Bazanov <54349899+ilibaz@users.noreply.github.com>
2025-06-05 11:10:37 +0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,14 @@
 import { Pool } from 'pg';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const pool = new Pool({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
     ssl: {
-        rejectUnauthorized: true,
+        rejectUnauthorized: isDevelopment ? false : true,
     }
 });
 
